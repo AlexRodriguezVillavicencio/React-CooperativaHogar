49b21808a5438cdac52cbf829f1f8276e579b7fc
Alex <arodriguezv@uni.pe>
2021-07-13 21:07:28 -0500

    agregando seccion transferencias y cambios en el navbar

---
+++ src/modules/pos/pages/Transferencias.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './../css/Transferencias.css'
+
+const Transferencias = () => {
+    return (
+        <>
+            <section className="contenedor">
+                <h1>Transferir a otra cuenta de cooperativa Hogar</h1>
+                <p>Ingrese los datos para ejecutar la transferencia</p>
+
+                <div className="formulario">
+                <form>
+                    <label className="origen" for="username">Cuenta Origen</label>
+                    <input type="number" placeholder=" 1234-5678-XXXX" />
+                    <label className="origen" for="password">Cuenta destino</label>
+                    <input type="number" placeholder="4564-5678-XXXX" />
+                
+                    <div className="transferencia">
+                    <label for="password">Monto a transferir</label>
+                    <input type="text" className="montoTrans"/>
+                    </div>
+                        
+                    <input type="submit" value="Transferir" />
+                </form>
+                
+                </div>
+            </section>  
+        </>
+    )
+}
+
+export default Transferencias
