d7b53f8f5460d77a152694f2b6e0943f63649ccb
Roberto Gonzalez Carranza <rgc00023@gmail.com>
2021-07-16 21:47:46 -0500

    creditos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
-import adminRouter from './modules/admin/adminRouter';
-import authRouter from './modules/auth/authRouter';
-import Bienvenida from './modules/pos/pages/Bienvenida';
-import PosRouter from './modules/pos/PosRouter';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import adminRouter from "./modules/admin/adminRouter";
+import authRouter from "./modules/auth/authRouter";
+import Bienvenida from "./modules/pos/pages/Bienvenida";
+import PosRouter from "./modules/pos/PosRouter";
+import Creditos from "./modules/pos/pages/Creditos";
 
 function App() {
   return (
     <>
-
       <Router>
- 
         <Switch>
-          <Route path="/bienvenida" component={Bienvenida} /> 
-          <Route path="/cuenta" component={PosRouter} /> 
-          <Route path="/admin" component={adminRouter} /> 
-          <Route path="/auth" component={authRouter}/>
-          <Redirect to="/bienvenida"/>
+          <Route path="/bienvenida" component={Bienvenida} />
+          <Route path="/cuenta" component={PosRouter} />
+          <Route path="/admin" component={adminRouter} />
+          <Route path="/auth" component={authRouter} />
+          <Redirect to="/bienvenida" />
         </Switch>
-      </Router> 
-
+      </Router>
     </>
   );
 }
--- src/modules/pos/components/Header.js
+++ src/modules/pos/components/Header.js
@@ -1,45 +1,48 @@
-import React from 'react';
+import React from "react";
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './../img/logo1_sinnom.png';
-import {NavDropdown, Navbar,Nav } from 'react-bootstrap';
-import { useState } from 'react';
-
- 
+import logo from "./../img/logo1_sinnom.png";
+import { Navbar, Nav } from "react-bootstrap";
+import { useState } from "react";
 
 const Header = () => {
+  const [dropdown, setDropdown] = useState(false);
 
-    const [dropdown, setDropdown] = useState(false);
+  const abrirCerrarDropdown = () => {
+    setDropdown(!dropdown);
+  };
 
-    const abrirCerrarDropdown = () => {
-      setDropdown(!dropdown)
-    }
+  return (
+    <>
+      <nav
+        className="navbar navbar-expand-lg navbar-dark"
+        style={{ backgroundColor: "#6ECAF1" }}
+      >
+        <div className="container-fluid">
+          <Navbar.Brand href="#">
+            <img
+              src={logo}
+              width="70"
+              height="60"
+              className="d-inline-block align-text-top"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle
+            onClick={abrirCerrarDropdown}
+            aria-controls="basic-navbar-nav"
+          />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/cuenta/mi_cuenta">MI CUENTA</Nav.Link>
+              <Nav.Link href="/cuenta/transferencias">TRANSFFERENCIAS</Nav.Link>
+              <Nav.Link href="/cuenta/creditos">CREDITOS</Nav.Link>
+              <Nav.Link href="/cuenta/inveriones">INVERSIONES</Nav.Link>
+              <Nav.Link href="/bienvenida">CERRAR SESION</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-    return (
-
-        <>
-            <nav className="navbar navbar-expand-lg navbar-dark" style= {{backgroundColor: '#6ECAF1'}}>
-                <div className="container-fluid">
-                <Navbar.Brand href="#"><img src={logo} width="70" height="60"
-                              className="d-inline-block align-text-top" /></Navbar.Brand>
-                    <Navbar.Toggle onClick={abrirCerrarDropdown} aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                          <Nav.Link href='/cuenta/mi_cuenta'>MI CUENTA</Nav.Link>
-                          <Nav.Link href='/cuenta/transferencias'>TRANSFFERENCIAS</Nav.Link>
-                            <NavDropdown variant="dark" title="CREDITOS" id="basic-nav-dropdown" style={{color: '#fff'}} >
-                            <NavDropdown.Item href="#action/3.1">SOLICITUDES</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">MIS CREDITOS</NavDropdown.Item>
-                            </NavDropdown>                            
-                          <Nav.Link href='/cuenta/mi_cuenta'>INVERSIONES</Nav.Link>
-                          <Nav.Link href='/bienvenida'>CERRAR SESION</Nav.Link> 
-                        </Nav>
-                      </Navbar.Collapse>
-                             
-                </div>
-            </nav> 
-  
-        </>
-    )
-}
-
-export default Header
+export default Header;
