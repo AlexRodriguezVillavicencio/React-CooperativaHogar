249ff6482ebfc80cd94a5f8d4d529747ef77e784
alex20204138 <83556616+alex20204138@users.noreply.github.com>
2021-07-18 02:29:01 -0500

    salvando cambios en posRouter.js

--- src/modules/pos/PosRouter.js
+++ src/modules/pos/PosRouter.js
@@ -3,7 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 import MiCuenta from './pages/MiCuenta';
 import Faucet from './pages/Faucet';
 import Encuentranos from './pages/Encuentranos';
-// import encuentranos from './pages/encuentranos';
+
 
 const PosRouter = () => {
     return (
@@ -11,12 +11,8 @@ const PosRouter = () => {
         <Switch>
             <Route path="/cuenta/mi_cuenta" component={MiCuenta} /> 
             <Route path="/cuenta/faucet" component={Faucet} />
-            <Route path="/cuenta/encuentranos" component={Encuentranos} />
-            
+            <Route path="/cuenta/encuentranos" component={Encuentranos} />            
         </Switch>
-        {/* <Switch>
-            <Route path="/cuenta/encuentranos" component={encuentranos} />
-        </Switch> */}
         </>
     )
 }
